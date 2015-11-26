41d4ac811b7fa96b8a0ffc569daaa9fe27860e74
justin999 <koichi.sato.aow@gmail.com>
2015-11-26 23:49:54 +0900

    hope it work

--- lib/daily_slack_report.rb
+++ lib/daily_slack_report.rb
@@ -1,5 +1,38 @@
 require "daily_slack_report/version"
+require "daily_slack_report/o_auth"
+require "slack"
 
 module DailySlackReport
   # Your code goes here...
+  def self.client
+    require "slack"
+    Slack.configure do |config|
+      config.token = "your_token"
+    end
+    client = Slack.client
+  end
+
+  def self.get_channel_id(client, channel_name = 'general')
+    params = {
+      token: "your_token",
+    }
+    list = client.channels_list params
+    channel_id = ""
+    list["channels"].each do |channel|
+      if channel["name"] == channel_name
+        channel_id = channel["id"]
+      end
+    end
+    channel_id
+  end
+
+  def self.get_chat_history(client, channels_id, target_date)
+    require 'Time'
+    options = {
+      channels_id: channels_id,
+      latest: Time.new(target_date.year, target_date.month, target_date.day,  0,  0,  0),
+      oldest: Time.new(target_date.year, target_date.month, target_date.day, 23, 59, 59)
+    }
+    client.channels_history options
+  end
 end
---
+++ lib/daily_slack_report/o_auth.rb
@@ -0,0 +1,8 @@
+module DailySlackReport
+  class OAuth
+    # codes
+    def self.say_hello
+      "hogehoge"
+    end
+  end
+end
